efbfcda76185592f0abf8b34ac10f781f140d978
vinchamp77 <vinchamp77@gmail.com>
2022-03-17 13:03:32 +1100

    Fix kotlin serialization plugin issues

--- app/src/main/java/vtsen/hashnode/dev/simplerestapidemo/data/KotlinSerdesMealCategoriesResponse.kt
+++ app/src/main/java/vtsen/hashnode/dev/simplerestapidemo/data/KotlinSerdesMealCategoriesResponse.kt
@@ -2,10 +2,7 @@ package vtsen.hashnode.dev.simplerestapidemo.data
 
 import kotlinx.serialization.Serializable
 
-//Note: Do not know how to get rid of this warning
-//Warning:(5, 1) kotlinx.serialization compiler plugin is not applied to the module, so this annotation
-//would not be processed. Make sure that you've setup your buildscript correctly and re-import project.
 @Serializable
 data class KotlinSerdesMealCategoriesResponse (
-    val categories: List<MoshiMealCategory>
+    val categories: List<KotlinSerdesMealCategory>
 )
