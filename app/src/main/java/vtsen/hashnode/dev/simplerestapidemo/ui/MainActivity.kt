afa6a92ba19ebce329deb030ee56f3a228e49978
vinchamp77 <vinchamp77@gmail.com>
2022-05-29 16:49:19 +1000

    Use viewModel() composable function

--- app/src/main/java/vtsen/hashnode/dev/simplerestapidemo/ui/MainActivity.kt
+++ app/src/main/java/vtsen/hashnode/dev/simplerestapidemo/ui/MainActivity.kt
@@ -3,12 +3,8 @@ package vtsen.hashnode.dev.simplerestapidemo.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import vtsen.hashnode.dev.simplerestapidemo.ui.navigation.BuildNavGraph
 import vtsen.hashnode.dev.simplerestapidemo.viewmodel.MainViewModel
@@ -16,19 +12,18 @@ import vtsen.hashnode.dev.simplerestapidemo.ui.theme.SimpleRestApiAppTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel by viewModels<MainViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            MainScreen(viewModel)
+            MainScreen(viewModel())
         }
     }
 }
 
 @Composable
 private fun MainScreen(viewModel: MainViewModel) {
+
     SimpleRestApiAppTheme {
         val navController = rememberNavController()
         BuildNavGraph(navController, viewModel)
